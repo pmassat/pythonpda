80cf56d97aa69a3d9b795b2f4eff7308043ad1af
Pierre Massat <pmassat@stanford.edu>
2020-04-28 19:12:57 -0700

    Analysis of neutrons data measured on 2019-02-14. Improved the creation of xFit objects by storing them in dictionaries.

--- Python_hacks/file_rename.py
+++ Python_hacks/file_rename.py
@@ -12,7 +12,8 @@ today = date.today()
 other_date = date(2020,3,19)
 
 # os.chdir(r'C:\Users\Pierre\Desktop\Postdoc\Writing\Articles\2020_TmVO4_model_nematicity\Ian-s_edits')
-os.chdir(r'C:\Users\Pierre\Desktop\Postdoc\TmVO4\TmVO4_NMR\2020-04_TmVO4_NMR')
+# os.chdir(r'C:\Users\Pierre\Desktop\Postdoc\TmVO4\TmVO4_NMR\2020-04_TmVO4_NMR')
+os.chdir(r'C:\Users\Pierre\Documents\Personnel\Famille\Famille_Massat')
 
 filenames = glob.glob('* *')# create list of filenames containing at least one whitespace
 
