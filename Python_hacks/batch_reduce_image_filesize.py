4dafdb69e271bc7cc120532aa0f40973f53b98a1
Pierre Massat <pmassat@stanford.edu>
2021-03-25 21:10:40 -0700

    More XRD data analysis.

--- Python_hacks/batch_reduce_image_filesize.py
+++ Python_hacks/batch_reduce_image_filesize.py
@@ -16,23 +16,22 @@ from PIL import Image
 today = date.today()
 other_date = date(2020, 7, 29)
 
-os.chdir(r'C:\Users\Pierre\Desktop\2021-03-11_phone_pics')
+os.chdir(r'C:\Users\Pierre\Desktop\2021-03-11_lab_pics')
 
 # create list of filenames containing at least one whitespace
-filenames = glob.glob('*.jpg')
+filenames = glob.glob('*.JPG')
 
 for filename in filenames:
-    foo = Image.open(filename)
-    print(foo.size)
     print(os.path.getsize(filename)/1024**2)
 
     # New file name replaces whitespaces with underscores
-    newfname = [filename.replace('_compressed.JPG', '.JPG')]
-    # newfname.append(newfname[-1].replace('.png', '.jpg'))
-    print(newfname[-1], '\n')
+    newfname = filename.replace('.JPG', '_Copy.jpg')
+    print(newfname)
 
+    foo = Image.open(filename)
+    print(foo.size, '\n')
     # Downsize the image with an ANTIALIAS filter (gives the highest quality)
     # if os.path.getsize(filename)/1024**2>1:
+        # foo.save(newfname, optimize=True, quality=50)
         # foo = foo.resize(tuple(int(x/2) for x in foo.size),Image.ANTIALIAS)
         # foo.save("path\\to\\save\\image_scaled.jpg",quality=95)
-        # foo.save(newfname[-1], optimize=True, quality=50)
